2cc86a0c784d6f0743251e96c6bea531cbe96485
Mark Raymond Jr <markrjr@live.com>
2019-04-20 17:21:20 -0500

    Some design work on the bill page.

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ app.get('/', function(req, res)
   res.render('home', { title : "Our Site", info : "NHK"});
 });
 
+app.get('/bill', function(req, res)
+{
+  res.render('home', { title : "Our Site", info : "NHK"});
+});
+
 const server = app.listen(3000, function() {
 
   console.log(`Server is listening on port ${ server.address().port }`)
